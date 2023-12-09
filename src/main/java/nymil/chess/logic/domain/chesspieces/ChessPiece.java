aeddfedf2d36dae61c15d085deb02831688ad318
Mattice Heyzak <mattice.heyzak@student.howest.be>
2023-12-09 23:19:45 +0100

    checking if a chesspiece has moved

--- src/main/java/nymil/chess/logic/domain/chesspieces/ChessPiece.java
+++ src/main/java/nymil/chess/logic/domain/chesspieces/ChessPiece.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 public abstract class ChessPiece {
     protected final ChessPieceColor color;
+    protected boolean hasMoved = false;
 
     public ChessPiece(String color) {
         this.color = ChessPieceColor.fromString(color);
@@ -15,4 +16,8 @@ public abstract class ChessPiece {
 
     public abstract Set<Move> getPossibleMoves(Map<ChessPiece, BoardLocation> currentBoardState);
     public abstract String toString();
+
+    public void setMoved() {
+        this.hasMoved = true;
+    }
 }
