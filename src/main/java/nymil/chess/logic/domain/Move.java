0c3dca79ee198c36fbd927b01c96091d15142fe2
Mattice Heyzak <mattice.heyzak@student.howest.be>
2023-12-09 23:22:57 +0100

    implemented move class

--- src/main/java/nymil/chess/logic/domain/Move.java
+++ src/main/java/nymil/chess/logic/domain/Move.java
@@ -1,4 +1,18 @@
 package nymil.chess.logic.domain;
 
 public class Move {
+    private final BoardLocation origin;
+    private final BoardLocation destination;
+    public Move(BoardLocation origin, BoardLocation destination) {
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public BoardLocation getOrigin() {
+        return origin;
+    }
+
+    public BoardLocation getDestination() {
+        return destination;
+    }
 }
