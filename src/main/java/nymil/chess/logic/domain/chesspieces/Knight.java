8a9246e3c11d1bbf41b531b813523ce071299965
Mattice Heyzak <mattice.heyzak@student.howest.be>
2023-12-24 17:26:52 +0100

    knight moves

--- src/main/java/nymil/chess/logic/domain/chesspieces/Knight.java
+++ src/main/java/nymil/chess/logic/domain/chesspieces/Knight.java
@@ -4,8 +4,7 @@ import nymil.chess.logic.domain.BoardLocation;
 import nymil.chess.logic.domain.ChessBoard;
 import nymil.chess.logic.domain.Move;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Knight extends ChessPiece {
 
@@ -15,11 +14,63 @@ public class Knight extends ChessPiece {
 
     @Override
     public Set<Move> getPossibleMoves() {
-        return null;
+        Set<Move> possibleMoves = new HashSet<>();
+
+        BoardLocation startingLocation = board.getLocation(this);
+
+        for (Map<String, Integer> coordDif : getPossibleMoveCoordDifs()) {
+            int possibleCol = startingLocation.getCol() + coordDif.get("colDif");
+            int possibleRow = startingLocation.getRow() + coordDif.get("rowDif");
+
+            if (!BoardLocation.inRange(possibleCol, possibleRow)) continue;
+            BoardLocation possibleLocation = new BoardLocation(possibleCol, possibleRow);
+
+            if (board.hasPieceOfColor(possibleLocation, this.color)) continue;
+            possibleMoves.add(new Move(startingLocation, possibleLocation));
+        }
+
+        return possibleMoves;
     }
 
     @Override
     public String toString() {
         return color + " knight";
+    }
+
+    public List<Map<String, Integer>> getPossibleMoveCoordDifs() {
+        return Arrays.asList(
+                new HashMap<>() {{
+                    put("colDif", 1);
+                    put("rowDif", 2);
+                }},
+                new HashMap<>() {{
+                    put("colDif", 2);
+                    put("rowDif", 1);
+                }},
+                new HashMap<>() {{
+                    put("colDif", 2);
+                    put("rowDif", -1);
+                }},
+                new HashMap<>() {{
+                    put("colDif", 1);
+                    put("rowDif", -2);
+                }},
+                new HashMap<>() {{
+                    put("colDif", -1);
+                    put("rowDif", -2);
+                }},
+                new HashMap<>() {{
+                    put("colDif", -2);
+                    put("rowDif", -1);
+                }},
+                new HashMap<>() {{
+                    put("colDif", -2);
+                    put("rowDif", 1);
+                }},
+                new HashMap<>() {{
+                    put("colDif", -1);
+                    put("rowDif", 2);
+                }}
+        );
     }
 }
